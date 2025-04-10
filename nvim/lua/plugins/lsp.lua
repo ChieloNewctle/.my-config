16884b7ce36239cbcf52f78a481eef200e35a2ef
Chielo Newctle <ChieloNewctle@gmail.com>
2025-04-10 21:03:03 +0800

    fix(nvim): use traefik v3 schema

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -12,6 +12,17 @@ return {
               format = { enable = false },
             },
           },
+          on_new_config = function(new_config)
+            new_config.settings.yaml.schemas = vim.tbl_deep_extend(
+              "force",
+              new_config.settings.yaml.schemas or {},
+              require("schemastore").yaml.schemas()
+            )
+            new_config.settings.yaml.schemas["https://json.schemastore.org/traefik-v2.json"] = nil
+          end,
+          schemas = {
+            ["https://json.schemastore.org/traefik-v3.json"] = { "traefik.yml", "traefik.yaml" },
+          },
         },
       },
     },
